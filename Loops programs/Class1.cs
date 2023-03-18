537fe855c3f08240105d4106e6d6a01cdc294158
deepak shinde <deepak shinde@DELL>
2023-03-18 14:57:46 +0530

    lcm

---
+++ Loops programs/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LcmGcd
+{
+    internal class Class1
+    {
+        public void lcm()
+        {
+            int lcm = 0; int gcd = 0;
+            Console.WriteLine("Enter two numbers");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = num2; i <= num1 * num2; i++)
+            {
+                if(i%num1==0 && i % num2 == 0)
+                {
+                    lcm = i;
+                    break;
+                }
+            }
+            Console.WriteLine("LCM = " + lcm);
+
+            gcd = num1 * num2 / lcm;
+            Console.WriteLine("gcd = " + gcd);
+        }
+    }
+}
--- Loops programs/Program.cs
+++ Loops programs/Program.cs
@@ -1,6 +1,8 @@
 ﻿//Loops All Programs
-
+using LcmGcd;
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 
 namespace abc
 {
@@ -9,7 +11,12 @@ namespace abc
 
         static void Main()
         {
-            //1)Reverse Programs
+            Class1 Class1 = new Class1();
+            Class1.lcm();
+
+
+
+            /*1)Reverse Programs
 
             Console.WriteLine("enter the  number");
             int Num=Convert.ToInt32( Console.ReadLine());
@@ -133,68 +140,164 @@ namespace abc
                 
             }*/
 
-           /* 7)C# Program to Convert Number in Characters
-            Console.WriteLine("Enter number :");                         //345
-            int Num = Convert.ToInt32(Console.ReadLine());
-            int r;int rev = 0;
-            if (Num >= 0)
+            /* 7)C# Program to Convert Number in Characters
+             Console.WriteLine("Enter number :");                         //345
+             int Num = Convert.ToInt32(Console.ReadLine());
+             int r;int rev = 0;
+             if (Num >= 0)
+             {
+                 r = Num % 10;
+                 rev = rev * 10 + r;
+                 Num = Num / 10;
+             }
+             Console.WriteLine(rev);                                          //543
+             Num = rev;
+             for(int i = 0; i <= Num; i++) {
+                 if (Num >= 0)
+                 {
+                     r = Num % 10;
+
+                     switch (r)
+                     {
+                         case 1:
+                             Console.Write("one ");
+                             break;
+                         case 2:
+                             Console.Write("two ");
+                             break;
+                         case 3:
+                             Console.Write("three ");
+                             break;
+                         case 4:
+                             Console.Write("four ");
+                             break;
+                         case 5:
+                             Console.Write("five ");
+                             break;
+                         case 6:
+                             Console.Write("six ");
+                             break;
+                         case 7:
+                             Console.Write("seven ");
+                             break;
+                         case 8:
+                             Console.Write("eight ");
+                             break;
+                         case 9:
+                             Console.Write("nine ");
+                             break;
+                         case 0:
+                             Console.Write("zero ");
+                             break;
+                         default:
+                             Console.Write("tttt ");
+                             break;
+                     }
+                     Num = Num / 10;
+                 }
+
+             }*/
+
+            /*7)show even number at given range
+             
+            Console.WriteLine("enter star of range");
+            int start = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter end of range");
+            int end = Convert.ToInt32(Console.ReadLine());
+            for (int i = start; i<= end; i++)
             {
-                r = Num % 10;
-                rev = rev * 10 + r;
-                Num = Num / 10;
-            }
-            Console.WriteLine(rev);                                          //543
-            Num = rev;
-            for(int i = 0; i <= Num; i++) {
-                if (Num >= 0)
+                if (i % 2 == 0)
                 {
-                    r = Num % 10;
-
-                    switch (r)
-                    {
-                        case 1:
-                            Console.Write("one ");
-                            break;
-                        case 2:
-                            Console.Write("two ");
-                            break;
-                        case 3:
-                            Console.Write("three ");
-                            break;
-                        case 4:
-                            Console.Write("four ");
-                            break;
-                        case 5:
-                            Console.Write("five ");
-                            break;
-                        case 6:
-                            Console.Write("six ");
-                            break;
-                        case 7:
-                            Console.Write("seven ");
-                            break;
-                        case 8:
-                            Console.Write("eight ");
-                            break;
-                        case 9:
-                            Console.Write("nine ");
-                            break;
-                        case 0:
-                            Console.Write("zero ");
-                            break;
-                        default:
-                            Console.Write("tttt ");
-                            break;
-                    }
-                    Num = Num / 10;
+                    Console.WriteLine($"even number= {i}");
                 }
+            }*/
+            /*8)show odd number at given range
 
+            Console.WriteLine("enter star of range");
+            int start = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter end of range");
+            int end = Convert.ToInt32(Console.ReadLine());
+            for (int i = start; i <= end; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine($"odd number= {i}");
+                }
+            }*/
+
+            /*9)show prime number at given range
+            bool isPrime = true;
+            Console.WriteLine("enter range");
+            int start = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 2; i <= start; i++)
+            {
+                for (int j = 2; j <= start; j++)
+                {
+                    if (i != j && i % j == 0)
+                    {
+                        isPrime = false;
+                    }
+                    if (isPrime = true)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }*/
+
+            /*10)swaping of two numbers
+
+            int a = 10, b = 20, c;
+            c = a;
+            a = b;
+            b = c;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            int x=5, y = 8;
+            x = x + y;  //13
+            y = x - y;   //5
+            x = x - y;   //
+
+            Console.WriteLine("value of a x ="+x);
+            Console.WriteLine("value of a y =" + y);*/
+
+            /*11) except any character and convert them from upper to lower and vise versa
+            Console.WriteLine("enter any character");
+            char ch =Convert.ToChar(Console.ReadLine());
+
+           int ch1 = Convert.ToInt32(ch);
+
+
+            if(ch1>=65 && ch1 <= 90)
+            {
+                Console.WriteLine( char.ToLower(ch));
+            }
+            if (ch1 >= 97 && ch1 <= 122)
+            {
+                Console.WriteLine(char.ToUpper(ch));
+            }*/
+
+            /*13)check number is perfect or not
+            int Num = 10;  int sum = 0; int copy = Num;
+            for(int i = 1; i <= Num; i++)
+            {
+                if (Num % i == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            if (sum == copy)
+            {
+                Console.WriteLine($"{Num} is a perfect number");
             }*/
 
 
+            }
         }
     }
-}
+
 
 
 
